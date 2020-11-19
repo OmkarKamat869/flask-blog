ebb756f5a39a2ea360b9113fa69fbd4bb8a59cc7
king674 <66460819+king674@users.noreply.github.com>
2020-11-19 12:05:39 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -152,7 +152,7 @@ def edit(sno):
                db.session.commit()
                return redirect('/edit/'+sno)
    post = Posts.query.filter_by(sno=sno).first()
-   return render_template('edit.html',params=params,posts=post)    
+   return render_template('edit.html',params=params,posts=post,sno=sno)    
 
 
 @app.route("/uploader", methods = ['GET', 'POST'])
